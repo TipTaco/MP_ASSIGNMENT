f4b1173b0c73bef86e443c988c26e4d8d887db68
adrian <adrian.shedley@gmail.com>
2019-08-23 17:07:57 +0800

    Initial Image loader

--- assignment.py
+++ assignment.py
@@ -1,3 +1,23 @@
+# Author: Adrian Shedley
+# date: 23 aug 2019
+#
+# Prepared for Machine Perception Assignment
+# assignmnet.py - the main file for the assignment with calls to both task one and two
+import cv2
 import imageloader as il
+import numpy as np
+il.init()
 
-il.load()
+
+# Take each of the building signs and extract ONE region and associated number
+def task1():
+    # start by using the first image
+    local = il.building_grey[0]
+    cv2.imshow("Grey", local)
+    cv2.waitKey(0)
+
+
+task1()
+
+
+
--- imageloader.py
+++ imageloader.py
@@ -4,20 +4,21 @@
 # Prepared for Machine Perception Assignment
 # imageloader.py - load in the images to train from.
 
-import numpy as np
 import os
 import cv2
-import glob
 from os import listdir
 
 building = list()
 building_name = list()
+building_grey = list()
+
 directional = list()
 directional_name = list()
+directional_grey = list()
 
 IMG_PATH = '/res/'
 
-def load():
+def init():
     path = os.getcwd() + IMG_PATH
     all_files = listdir(path)
     for file in all_files:
@@ -27,10 +28,12 @@ def load():
             if img is not None:
                 if file.startswith("BS"):
                     building.append(img)
+                    building_grey.append(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
                     building_name.append(file)
                 elif file.startswith("DS"):
                     directional.append(img)
+                    directional_grey.append(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
                     directional_name.append(file)
 
+
     print("[Image Loader] Loaded", len(building), "building signs and", len(directional), "directional signs")
-    
