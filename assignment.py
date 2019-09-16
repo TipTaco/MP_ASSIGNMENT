e7c7eb59cc99d22d6e363f8caf35987d8bfa5722
tiptaco <adrian.shedley@gmail.com>
2019-09-16 11:25:19 +0800

    Training data loaded

--- assignment.py
+++ assignment.py
@@ -8,16 +8,67 @@ import imageloader as il
 import numpy as np
 il.init()
 
+def normalise(A):
+    return (A - A.min() / (A.max() - A.min()))
 
 # Take each of the building signs and extract ONE region and associated number
+def task2():
+    mser = cv2.MSER_create(20, 200)
+
+    for i, img in enumerate(il.directional):
+        local = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gaus = cv2.GaussianBlur(local, (3,3), 0.5)
+        media = cv2.medianBlur(local, 3)
+        bina = np.zeros_like(local)
+        cv2.threshold(media, 120, 255, cv2.THRESH_BINARY, bina)
+        # bina = cv2.medianBlur(bina, 7)
+        gaus = cv2.morphologyEx(gaus, cv2.MORPH_ERODE, np.ones((3,3)))
+        cv2.imshow("bina", gaus)
+
+        features, boxes = mser.detectRegions(gaus)
+
+        for feat in boxes:
+            cv2.rectangle(img, (feat[0], feat[1]), (feat[0] + feat[2], feat[1] + feat[3]), (0, 255, 0),
+                          thickness=2)
+        cv2.imshow("Img" + str(i), img)
+        cv2.waitKey(0)
+
 def task1():
     # start by using the first image
     local = il.building_grey[0]
     cv2.imshow("Grey", local)
+    # cv2.waitKey(0)
+
+    media = cv2.medianBlur(local, 3)
+    cv2.imshow("Median", media)
+
+    bina = np.zeros_like(local)
+    cv2.threshold(media, 150, 255, cv2.THRESH_BINARY, bina)
+
+    bina = cv2.medianBlur(bina, 7)
+    # bina = cv2.erode(bina, (9, 9))
+
+   # orig = np.zeros_like(local)
+   # cv2.threshold(media, 160, 255, cv2.THRESH_OTSU, orig)
+
+   # output = normalise(np.float64(orig - orig ^ bina))
+
+    mser = cv2.MSER_create(20, 200)
+    features, boxes = mser.detectRegions(bina)
+
+    print(features)
+
+    for feat in boxes:
+        cv2.rectangle(il.building[0], (feat[0], feat[1]), (feat[0]+feat[2], feat[1]+feat[3]), (0,255,0), thickness=1)
+
+    cv2.imshow("binary", bina)
+    # cv2.imshow("otsu", orig)
+    cv2.imshow("outpit", il.building[0])
+    # cv2.imshow("subtract", output)
+ ##
     cv2.waitKey(0)
 
-
-task1()
+task2()
 
 
 
--- imageloader.py
+++ imageloader.py
@@ -16,7 +16,11 @@ directional = list()
 directional_name = list()
 directional_grey = list()
 
+training = list()
+
 IMG_PATH = '/res/'
+TRAIN_PATH = '/res/training/original'
+NUMBERS = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'LeftArrow', 'RightArrow']
 
 def init():
     path = os.getcwd() + IMG_PATH
@@ -35,5 +39,20 @@ def init():
                     directional_grey.append(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
                     directional_name.append(file)
 
+    path = os.getcwd() + TRAIN_PATH
+    all_files = listdir(path)
+
+    for i in range(len(NUMBERS)):
+        training.append(list())
+
+    for file in all_files:
+        # print(file)
+        if file.endswith(".jpg"):
+            img = cv2.imread(path + '/' + file)
+            if img is not None:
+                for i, nn in enumerate(NUMBERS):
+                    if file.startswith(nn):
+                        training[i].append(file)
+                        break
 
     print("[Image Loader] Loaded", len(building), "building signs and", len(directional), "directional signs")
