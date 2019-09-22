71846b6456a813479146a0dedb023bff64cc9f6c
adrian <adrian.shedley@gmail.com>
2019-09-23 00:02:50 +0800

    Attempting SVM

--- assignment.py
+++ assignment.py
@@ -75,6 +75,7 @@ def task1(index):
         roi = il.building_grey[index][feat[1]:feat[1]+feat[3], feat[0]:feat[0]+feat[2]]
         if roi.shape[0]/roi.shape[1] > 2.0:
             roi = cv2.copyMakeBorder(roi, 0, 0, int(roi.shape[1] / 4), int(roi.shape[1] / 4), cv2.BORDER_REPLICATE)
+        #claass, dist = il.classify(roi.astype('float32'))
         claass, dist = il.classify(roi.astype('float32'))
         print('distance', np.sum(dist))
         if (np.sum(dist) < 80000000):
@@ -86,13 +87,131 @@ def task1(index):
     # cv2.imshow("otsu", orig)
     cv2.imshow("outpit", rgb)
     # cv2.imshow("subtract", output)
- ##
+
     cv2.waitKey(0)
 
 
-#task2();
-for i in range(len(il.building_grey)):
-    task1(i)
+def task1_1(index):
+    # start by using the first image
+    number = cv2.imread('res/training/original/One1.jpg')
+    number_grey = cv2.cvtColor(number, cv2.COLOR_BGR2GRAY)
 
+    grey = il.building_grey[index]
+    rgb = il.building[index].copy()
+    gaus = cv2.GaussianBlur(grey, (3,3), 0)
+    cv2.imshow("gaus", gaus)
+
+    thresh = cv2.threshold(grey, 120, 255, cv2.THRESH_BINARY)[1]
+    cv2.imshow("thr", thresh)
+
+    out = grey.copy()
+    out[thresh != 255] = 0
+
+    out = cv2.medianBlur(out, 5)
+    cv2.imshow('out', out)
+
+    out = cv2.threshold(out, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+    cv2.imshow('out', out)
+
+    out = cv2.GaussianBlur(out, (5,5), 1)
+
+    mser = cv2.MSER_create(5, 40)
+    features, boxes = mser.detectRegions(out)
+
+    adapt = cv2.adaptiveThreshold(grey, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 125, 1)
+    cv2.imshow('adapt', adapt)
+
+    # _, ccl = cv2.connectedComponents(out, connectivity=4)
+    #ccl = np.uint8(cv2.normalize(ccl, None, 0, 255, cv2.NORM_MINMAX))
+    #cv2.imshow('ccl', ccl)
+
+    output = cv2.connectedComponentsWithStats(adapt, 4, cv2.CV_32S)
+    # Get the results
+    # The first cell is the number of labels
+    num_labels = output[0]
+    # The second cell is the label matrix
+    labels = output[1]
+    # The third cell is the stat matrix
+    stats = output[2]
+    # The fourth cell is the centroid matrix
+    centroids = output[3]
+
+    ccl = np.uint8(cv2.normalize(labels, None, 0, 255, cv2.NORM_MINMAX))
+    cv2.imshow('ccl', ccl)
+
+
+    for feat in stats:
+        if feat[cv2.CC_STAT_AREA] < 80: continue
+        roi = il.building_grey[index][feat[1]:feat[1]+feat[3], feat[0]:feat[0]+feat[2]]
+        if roi.shape[0]/roi.shape[1] > 2.0:
+            roi = cv2.copyMakeBorder(roi, 0, 0, int(roi.shape[1] / 4), int(roi.shape[1] / 4), cv2.BORDER_REPLICATE)
+        #claass, dist = il.classify(roi.astype('float32'))
+        claass, dist = il.classify(roi.astype('float32'))
+        print('distance', np.sum(dist))
+        if (np.sum(dist) < 70000000):
+            cv2.rectangle(rgb, (feat[0], feat[1]), (feat[0] + feat[2], feat[1] + feat[3]), (0, 255, 0), thickness=2)
+            cv2.putText(rgb, str(claass), (feat[0], feat[1]-5), cv2.FONT_HERSHEY_SIMPLEX, 1, thickness=2, color=(120, 255, 0))
+
+
+    cv2.imshow('rect', rgb)
+    cv2.waitKey(0)
+
+
+def task1_2(index):
+    # start by using the first image
+    number = cv2.imread('res/training/original/One1.jpg')
+    number_grey = cv2.cvtColor(number, cv2.COLOR_BGR2GRAY)
+
+    grey = il.building_grey[index]
+    rgb = il.building[index].copy()
+    gaus = cv2.GaussianBlur(grey, (3,3), -0.5)
+    cv2.imshow("gaus", gaus)
+
+    textKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 30))
+    secondPassKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 2))
+    output = []
+    inv_grey = np.uint8(grey * -1.0 + 255)
+    cv2.imshow('inverse', inv_grey)
+    blackhat = cv2.morphologyEx(inv_grey, cv2.MORPH_BLACKHAT, textKernel)
+    cv2.imshow('blackhot', blackhat)
+
+    thresh = cv2.threshold(blackhat, 100, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.medianBlur(thresh, 3)
+    #thresh = cv2.GaussianBlur(thresh, (5,5), 1)
+    #thresh = cv2.morphologyEx(thresh, cv2.MORPH_DILATE, secondPassKernel)
+
+    #new_grey = grey.copy()
+   #new_grey[thresh == 0] = 0
+
+    cv2.imshow('thre', thresh)
+    #cv2.imshow('out', new_grey)
+
+    #new_grey = cv2.threshold(thresh, 150, 255, cv2.THRESH_BINARY)[1]
+
+    #cv2.imshow('out2', new_grey)
+
+    mser = cv2.MSER_create(5, 40)
+    features, boxes = mser.detectRegions(thresh)
+
+    for feat in boxes:
+        roi = il.building_grey[index][feat[1]:feat[1]+feat[3], feat[0]:feat[0]+feat[2]]
+        if roi.shape[0]/roi.shape[1] > 2.0:
+            roi = cv2.copyMakeBorder(roi, 0, 0, int(roi.shape[1] / 4), int(roi.shape[1] / 4), cv2.BORDER_REPLICATE)
+        #claass, dist = il.classify(roi.astype('float32'))
+        claass, dist = il.classifySVM(roi.astype('float32'))
+        print('distance', np.sum(dist))
+        if (np.sum(dist) < 70000000):
+            cv2.rectangle(rgb, (feat[0], feat[1]), (feat[0] + feat[2], feat[1] + feat[3]), (0, 255, 0), thickness=2)
+            cv2.putText(rgb, str(claass), (feat[0], feat[1]-5), cv2.FONT_HERSHEY_SIMPLEX, 1, thickness=2, color=(120, 255, 0))
+
+    cv2.imshow('rect', rgb)
+    cv2.waitKey(0)
+
+#task2();
+#for i in range(len(il.building_grey)):
+    #task1(i)
+
+for i in range(len(il.building_grey)):
+    task1_2(i)
 
 
--- imageloader.py
+++ imageloader.py
@@ -21,8 +21,10 @@ training = list()
 training_aug = list()
 
 kNN = cv2.ml.KNearest_create()
-
+svm = cv2.ml.SVM_create()
 aNN = cv2.ml.ANN_MLP_create()
+
+FEATURES = 13
 
 IMG_PATH = '/res/'
 TRAIN_VAR_PATH = '/res/training/augmented'
@@ -50,7 +52,7 @@ def init():
     path = os.getcwd() + TRAIN_PATH
     all_files = listdir(path)
 
-    for i in range(len(NUMBERS)):
+    for i in range(len(NUMBERS) + 1):
         training.append(list())
         training_aug.append(list())
 
@@ -76,21 +78,27 @@ def init():
                             training_aug[i].append(img[:40, :28])
                             break
 
+    for i in range(200):
+        new_img = np.random.randint(256, size=(40, 28), dtype=np.uint8)
+        training_aug[12].append(new_img)
+        #cv2.imshow('new_img', new_img)
+        #cv2.imwrite('res/training/noise/noise.' + str(i) + '.jpg', new_img)
+
     print("[Image Loader] Loaded", len(building), "building signs and", len(directional), "directional signs")
 
     # Classifier
     set, lab = training_set()
 
-    labels = np.zeros((set.shape[0], 12), dtype=np.float32)
+    labels = np.zeros((set.shape[0], FEATURES), dtype=np.float32)
     for i, labl in enumerate(lab):
         labels[i][int(labl)] = 1.0
 
     #aNN.load("aNN1.dat")
-    aNN.setLayerSizes(np.array([28*40, 100, 25, 12], dtype=np.uint16))
-    aNN.setTrainMethod(cv2.ml.ANN_MLP_BACKPROP)
-    aNN.setActivationFunction(cv2.ml.ANN_MLP_SIGMOID_SYM)
-    aNN.train(set, cv2.ml.ROW_SAMPLE, labels)
-    aNN.save("aNN1.dat")
+    #aNN.setLayerSizes(np.array([28*40, 100, 25, 12], dtype=np.uint16))
+    #aNN.setTrainMethod(cv2.ml.ANN_MLP_BACKPROP)
+    #aNN.setActivationFunction(cv2.ml.ANN_MLP_SIGMOID_SYM)
+    #aNN.train(set, cv2.ml.ROW_SAMPLE, labels)
+    #aNN.save("aNN1.dat")
 
 
     # Initiate kNN, train the data, then test it with test data for k=1
@@ -99,40 +107,88 @@ def init():
 
     test, test_lab = test_set()
     _, result, _, _ = kNN.findNearest(test, k=5)
-    result = result.reshape(1200)
+    result = result.reshape(FEATURES * 100)
     print(result.shape, test.shape, test_lab.shape)
     print('kNN classifier for augmented numbers', accuracy(test_lab, result))
 
-    labels_test = np.zeros((set.shape[0], 12), dtype=np.float32)
+    labels_test = np.zeros((set.shape[0], FEATURES), dtype=np.float32)
     for i, labl in enumerate(test_lab):
         labels_test[i][int(labl)] = 1.0
 
-    o, res = aNN.predict(test)
+    ## hog test with SVM
+    hogdata, hoglab = training_hog()
 
-    crect = 0
-    for i, r in enumerate(res):
-        mm = np.argmax(r)
-        if mm == int(test_lab[i]):
-            crect += 1
-        else:
-            print('expect', int(test_lab[i]), 'got', mm)
+    svm.setType(cv2.ml.SVM_C_SVC)
+    svm.setC(2.67)
+    svm.setGamma(5.383)
+    svm.setKernel(cv2.ml.SVM_DEGREE)
 
-    print(crect / len(test))
+    svm.train(hogdata, cv2.ml.ROW_SAMPLE, hoglab)
+    svm.save('svm_data.dat')
 
+    testhog, testhoglab = test_hog()
+    print(testhog.shape)
 
+    _, result = svm.predict(testhog)
+    result = result.reshape(1300)
+    print('SVM classifier for augmented numbers', accuracy(testhoglab, result))
+
+    #hh = np.float32(hog(training_aug[12][0])).reshape(-1, 64)
+    hh, _= classifySVM(training_aug[12][0])
+    cv2.imshow('random', training_aug[12][0])
+    print('static is', hh)
+
+    #o, res = aNN.predict(test)
+
+    #crect = 0
+    #for i, r in enumerate(res):
+    #    mm = np.argmax(r)
+    #    if mm == int(test_lab[i]):
+    #        crect += 1
+    #    else:
+    #        print('expect', int(test_lab[i]), 'got', mm)
+
+   # print(crect / len(test))
    # cv2.imshow('test1 is a ' + str(classify(test[340].reshape(40, 28))), test[340].reshape(40, 28))
     #cv2.waitKey(0)
 
+def hog(img):
+    gradX = cv2.Sobel(img, cv2.CV_32F, 1, 0)
+    gradY = cv2.Sobel(img, cv2.CV_32F, 0, 1)
+    magnitude, angle = cv2.cartToPolar(gradX, gradY)
+
+    BINS = 16
+    bins = np.int32(BINS * (angle / (2*np.pi)))
+
+    C = 14
+    R = 20
+
+    bin_cells = bins[:R, :C], bins[R:, :C], bins[:R, C:], bins[R:, C:]
+    mag_cells = magnitude[:R, :C], magnitude[R:, :C], magnitude[:R, C:], magnitude[R:, C:]
+    hists = [np.bincount(b.ravel(), m.ravel(), BINS) for b, m in zip(bin_cells, mag_cells)]
+    hist = np.hstack(hists)
+    return hist
+
 def deskew(img):
-    SZ = 20
     affine_flags = cv2.WARP_INVERSE_MAP | cv2.INTER_LINEAR
     m = cv2.moments(img)
     if abs(m['mu02']) < 1e-2:
         return img.copy()
     skew = m['mu11']/m['mu02']
-    M = np.float32([[1, skew, -0.5*SZ*skew], [0, 1, 0]])
+    M = np.float32([[1, skew, -0.5*28*skew], [0, 1, 0]])
     img = cv2.warpAffine(img,M,(28, 40),flags=affine_flags)
     return img
+
+def classifySVM(img):
+    img_resize = cv2.resize(img, (22, 34), interpolation=cv2.INTER_NEAREST)
+    img_resize = cv2.copyMakeBorder(img_resize, 3, 3, 3, 3, cv2.BORDER_CONSTANT)
+    img_resize = cv2.threshold(img_resize, 120, 255, cv2.THRESH_BINARY)[1]
+
+    hh = np.float32(hog(img_resize)).reshape(-1, 64)
+    #print(hh[0])
+    print(hh.shape)
+    ret, result = svm.predict(hh)
+    return int(result[0][0]), ret
 
 def classifyANN(img):
     cv2.imshow('begin', img.astype('uint8'))
@@ -156,19 +212,42 @@ def classify(img):
     img_resize = deskew(img_resize)
     cv2.imshow('resize', img_resize.astype('uint8'))
     cv2.waitKey(1)
-    print(img_resize.shape)
+    #print(img_resize.shape)
     vect = np.array(img_resize.reshape(1, 40 * 28))
-    print(vect)
+    #print(vect)
     _, result, _, dist = kNN.findNearest(vect, k=5)
     return int(result[0][0]), dist
 
 def test_set():
-    return subset(100, 12, 100)
+    return subset(100, 13, 100)
 
 def training_set():
-    return subset(0, 12, 100)
+    return subset(0, 13, 100)
 
-def subset(low, digits=12, samples=100):
+def training_hog():
+    return subset_hog(0, 13, 100)
+
+def test_hog():
+    return subset_hog(100, 13, 100)
+
+def subset_hog(low, digits, samples=100):
+    set = np.zeros((samples * digits, 64), dtype=np.float32)
+    set_labels = np.zeros((samples * digits), dtype=int)
+
+    matrixAug = np.array(training_aug)
+    # print(training_aug)
+
+    for i in range(digits):
+        for j in range(samples):
+            _, im = cv2.threshold(matrixAug[i][low + j], 120, 255, cv2.THRESH_BINARY)
+
+            hh = hog(im)
+            set[i * samples + j] = hh.reshape(64)
+            set_labels[i * samples + j] = int(i)
+
+    return set, set_labels
+
+def subset(low, digits, samples=100):
     set = np.zeros((samples*digits, 40*28), dtype=np.float32)
     set_labels = np.zeros((samples*digits), dtype=np.float32)
 
@@ -184,7 +263,6 @@ def subset(low, digits=12, samples=100):
     #cv2.imshow('test;,', set[0].reshape(40, 28))
     #cv2.waitKey(0)
     #print(set.shape, set_labels.shape)
-
     return set, set_labels
 
 def accuracy(labels, result):
