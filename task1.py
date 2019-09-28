95f593bf293671ea0fd1fb9da3a9ffea02d66795
tiptaco <adrian.shedley@gmail.com>
2019-09-28 13:25:33 +0800

    task1 update

--- task1.py
+++ task1.py
@@ -1,4 +1,4 @@
-# Authr Adrian Shedley, Revised 27 Sep 2019
+ # Authr Adrian Shedley, Revised 27 Sep 2019
 
 import cv2
 import imageloader as il
@@ -29,11 +29,16 @@ def task1(img):
 
     im3 = img.copy()
 
+    bounds = list()
+
     for i in conts:
         x, y, w, h = cv2.boundingRect(i)
         if w*h > 300 and w*h < 10000:
             if h > 1.1 * w and h < 4*w:
+                bounds.append((x,y,w,h))
                 cv2.rectangle(im3, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    group(bounds, grey.shape[1], grey.shape[0])
 
     cv2.imshow('bound', im3)
 
@@ -44,6 +49,34 @@ def task1(img):
     #cv2.imshow('median', median)
 
     cv2.waitKey(0)
+
+
+def group(list, width, height):
+    target = 3
+    inc = 2
+    offset = 10
+
+    for i in range(offset, height, inc):
+        new_list = getYBetween(list, height - i*inc, height - i*inc - offset)
+        blank = np.zeros((height, width), dtype=np.uint8)
+
+        blank = cv2.cvtColor(blank, cv2.COLOR_GRAY2BGR)
+        cv2.rectangle(blank, (0, height - i*inc), (0, height - i*inc - offset), color=(255, 0, 0), thickness=1)
+
+        for ele in new_list:
+            cv2.rectangle(blank, (ele[0], ele[1]), (ele[0] + ele[2], ele[1] + ele[3]), color=(0,250,0), thickness=2)
+
+        cv2.imshow('black', blank)
+        cv2.waitKey(0)
+
+
+def getYBetween(bounds, y_upper, y_lower):
+    output = list()
+
+    for ele in bounds:
+        if ele[1] > y_lower and ele[1] <= y_upper:
+            output.append(ele)
+    return output
 
 
 # Run the function classification of each of the building images
